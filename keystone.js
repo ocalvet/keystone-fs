b79b9cbeb538ce5c892c9476b06e218b9752e728
Ovidio R. Calvet <calveto@gmail.com>
2018-07-07 04:01:10 -0400

    Add index route

--- keystone.js
+++ keystone.js
@@ -3,11 +3,14 @@ var keystone = require('keystone');
 keystone.init({
   'cookie secret': 'secure string goes here',
   'name': 'keystone-fs',
+  'views': 'templates/views',
+  'view engine': 'pug',
   'user model': 'User',
   'auto update': true,
   'auth': true,
 });
 
+keystone.set('routes', require('./routes'));
 keystone.import('models');
 
 keystone.start();
