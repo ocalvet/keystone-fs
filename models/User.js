b032a1d8efe183a1571783b2990eb40c646f526c
Ovidio R. Calvet <calveto@gmail.com>
2018-07-06 23:26:10 -0400

    Make admin UI work

---
+++ models/User.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone');
+
+var User = new keystone.List('User');
+
+User.add({
+  displayName: { type: String },
+  email: { type: keystone.Field.Types.Email, unique: true },
+  password: { type: keystone.Field.Types.Password },
+});
+
+User.schema.virtual('canAccessKeystone').get(function () {
+  return true;
+});
+
+User.defaultColumns = 'id, displayName, email';
+
+User.register();
