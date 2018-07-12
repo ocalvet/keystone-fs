f30290a803ff528a87791de303d8cdf3486c9060
Ovidio Calvet <calveto@gmail.com>
2018-07-11 22:51:33 -0400

    Change gallery properties

--- components/Gallery.js
+++ components/Gallery.js
@@ -10,12 +10,14 @@ export default (props) => {
     <div>
       <h4 style={{ marginBottom: 15 }}>{gallery.name}</h4>
       <GridList cols={2.5}>
-        {gallery.images.map(image => (
+        {gallery.images.map(image => {
+          console.log('image', image);
+          return (
           <GridListTile key={image._id}>
             <img src={image.secure_url} alt={image.secure_url} />
-            <GridListTileBar title={image.secure_url}/>
+            <GridListTileBar title={image.public_id}/>
           </GridListTile>
-        ))}
+        )})}
       </GridList>
     </div>
   );
